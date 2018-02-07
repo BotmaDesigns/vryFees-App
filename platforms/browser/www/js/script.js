97870d06df41e6906d8e1ee90130388118f49e31
BotmaDesigns <BotmaDesigns@users.noreply.github.com>
2018-02-07 14:07:02 +0200

    working on geo
    trying to getgeolocation to work for venues

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -7,3 +7,27 @@
         x.className = "menu";
     }
 }
+
+
+function createTable(tableData) {
+  var table = document.createElement('table');
+  var tableBody = document.createElement('tbody');
+  table.className="venueSchedule";
+
+  tableData.forEach(function(rowData) {
+    var row = document.createElement('tr');
+
+    rowData.forEach(function(cellData) {
+      var cell = document.createElement('td');
+      cell.appendChild(document.createTextNode(cellData));
+      row.appendChild(cell);
+    });
+
+    tableBody.appendChild(row);
+  });
+
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
+}
+
+/*createTable([["row 1, cell 1", "row 1, cell 2"], ["row 2, cell 1", "row 2, cell 2"]]);*/
