cbac048bc4f9f456e94b2928e5753bf7cbe9bb25
BotmaDesigns <BotmaDesigns@users.noreply.github.com>
2018-02-07 08:36:57 +0200

    vryFees app test
    first upload of vryFees app

---
+++ www/js/script.js
@@ -0,0 +1,9 @@
+
+	function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+        x.className += " showMenu";
+    } else {
+        x.className = "menu";
+    }
+}
